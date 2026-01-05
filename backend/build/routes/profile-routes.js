528efdcddaf8e5cdae4b40d6015b57cc1e24ca16
lucas <lucaswoo583@gmail.com>
2026-01-05 15:37:23 -0700

    remove typescript build folder

--- backend/build/routes/profile-routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { protectedroute } from "../middleware/auth-validators.js";
-import { getProfilePictures, selectProfilePicture } from "../handlers/profile-routes.js";
-const router = Router();
-router.get("/photos", protectedroute, getProfilePictures);
-router.post("/change-photo", protectedroute, selectProfilePicture);
-export default router;
--- backend/build/routes/test-routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { testReqUser } from "../handlers/test-routes.js";
-//we gotta change any routes that is only updating req.user and not in the db itself
-const router = Router();
-router.get("/", testReqUser);
-export default router;
