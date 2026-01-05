e81e00c16dcea85f87864e2c8aa0d204c99b806c
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-01-05 15:37:23 -0700

    remove typescript build folder

--- backend/build/database/models/Usernames.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const usernameSchema = new mongoose.Schema({
-    username: {
-        type: mongoose.Schema.Types.String,
-        required: true,
-        unique: true
-    },
-});
-export const UsernamesModel = mongoose.model("Usernames", usernameSchema);
